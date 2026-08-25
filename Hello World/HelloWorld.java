c0d5274e652f394a51b9e2cef8d99dbdc59b0cc2
Dan Murlaga <danmurlaga@icloud.com>
2026-08-25 12:06:02 -0500

    Added second print line

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -1,6 +1,7 @@
+// Dan Murlaga
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        //dfghjk
+        System.out.println("This is my first Java program");
     }
 }
